2c1c3ccd7354759455f59213c66632d326fc0025
Planet <43352020+PlanetNoobGotHacked@users.noreply.github.com>
2021-06-26 02:11:39 -0400

    sha256 instead of aes

--- main.go
+++ main.go
@@ -1,17 +1,13 @@
 package main
 
 import (
-	"crypto/aes"
-	"crypto/cipher"
 	"crypto/md5"
-	"crypto/rand"
+	"crypto/sha256"
 	"encoding/hex"
 	"errors"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"log"
-	rand2 "math/rand"
 	"os"
 	"strings"
 	"sync"
@@ -22,39 +18,6 @@ func createHash(key string) string {
 	hasher := md5.New()
 	hasher.Write([]byte(key))
 	return hex.EncodeToString(hasher.Sum(nil))
-}
-
-func encrypt(data []byte, passphrase string) []byte {
-	block, _ := aes.NewCipher([]byte(createHash(passphrase)))
-	gcm, err := cipher.NewGCM(block)
-	if err != nil {
-		panic(err.Error())
-	}
-	nonce := make([]byte, gcm.NonceSize())
-	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
-		panic(err.Error())
-	}
-	ciphertext := gcm.Seal(nonce, nonce, data, nil)
-	return ciphertext
-}
-
-func decrypt(data []byte, passphrase string) []byte {
-	key := []byte(createHash(passphrase))
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		panic(err.Error())
-	}
-	gcm, err := cipher.NewGCM(block)
-	if err != nil {
-		panic(err.Error())
-	}
-	nonceSize := gcm.NonceSize()
-	nonce, ciphertext := data[:nonceSize], data[nonceSize:]
-	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
-	if err != nil {
-		panic(err.Error())
-	}
-	return plaintext
 }
 
 type stack struct {
@@ -86,17 +49,6 @@ func (s *stack) Pop() (int, error) {
 	res := s.s[l-1]
 	s.s = s.s[:l-1]
 	return res, nil
-}
-
-func encryptFile(filename string, data []byte, passphrase string) {
-	f, _ := os.Create(filename)
-	defer f.Close()
-	f.Write(encrypt(data, passphrase))
-}
-
-func decryptFile(filename string, passphrase string) []byte {
-	data, _ := ioutil.ReadFile(filename)
-	return decrypt(data, passphrase)
 }
 
 func getOpcode(instruction string) int {
@@ -221,7 +173,12 @@ func main() {
 
 			fmt.Println(string(poppedValue))
 		case 5:
-			encryptFile(os.Args[1], data, string(rand2.Int()))
+			h := sha256.New()
+			h.Write([]byte(os.Args[1]))
+
+			if err := os.WriteFile(os.Args[1], []byte(h.Sum(nil)), 0666); err != nil {
+				log.Fatal(err)
+			}
 		default:
 
 		}
